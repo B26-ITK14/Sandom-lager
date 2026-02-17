c14e9ff5e6664c79e6881e382e0936b68437e3a1
Andreas Skaarberg <andreas.skaarberg@gmail.com>
2026-02-17 21:53:24 +0100

    Created schema for users and user_locations

--- sandom-lager/backend/src/db/schema.sql
+++ sandom-lager/backend/src/db/schema.sql
@@ -3,7 +3,7 @@
 -- This file defines the database schema for the Sandom Lager application.
 -- It includes the necessary tables and their relationships.
 
--- Locations
+-- LOCATIONS --
 CREATE TABLE IF NOT EXISTS locations (
     id SERIAL PRIMARY KEY,
     name TEXT(255) NOT NULL UNIQUE,
@@ -13,3 +13,22 @@ CREATE TABLE IF NOT EXISTS locations (
 INSERT INTO locations (name)
 VALUES ('Sandom Retreatsenter'),
        ('Tomasgården');
+
+-- USERS --
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    auth0_id TEXT NOT NULL UNIQUE,
+    email TEXT NOT NULL UNIQUE,
+    name TEXT NOT NULL,
+    role TEXT NOT NULL DEFAULT 'user', -- 'user', 'admin', 'manager'
+    created_at TIMESTAMPTZ DEFAULT NOW(),
+);
+
+-- USER LOCATION ACCESS --
+CREATE TABLE IF NOT EXISTS user_locations (
+    id SERIAL PRIMARY KEY,
+    user_id INT REFERENCES users(id) ON DELETE CASCADE,
+    location_id INT REFERENCES locations(id) ON DELETE CASCADE,
+    access_status TEXT NOT NULL DEFAULT 'pending', -- 'pending', 'approved', 'denied'
+    UNIQUE(user_id, location_id)
+);
