f38293a7205bfc869061f1a85b6a7c4f3044e8dc
Andreas Skaarberg <andreas.skaarberg@gmail.com>
2026-02-17 22:18:11 +0100

    Created ingredients, recipes, recipe ingredients, allergens, recipe_allergens schemas

--- sandom-lager/backend/src/db/schema.sql
+++ sandom-lager/backend/src/db/schema.sql
@@ -32,3 +32,50 @@ CREATE TABLE IF NOT EXISTS user_locations (
     access_status TEXT NOT NULL DEFAULT 'pending', -- 'pending', 'approved', 'denied'
     UNIQUE(user_id, location_id)
 );
+
+-- INGREDIENTS --
+CREATE TABLE IF NOT EXISTS ingredients (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL UNIQUE,
+    unit TEXT NOT NULL, -- 'kg', 'liter'
+);
+
+-- RECIPES --
+CREATE TABLE IF NOT EXISTS recipes (
+    id SERIAL PRIMARY KEY,
+    location_id INT REFERENCES locations(id) ON DELETE CASCADE,
+    title TEXT NOT NULL,
+    category TEXT NOT NULL,
+    instructions TEXT, 
+    created_at TIMESTAMPTZ DEFAULT NOW(),
+);
+
+-- RECIPE INGREDIENTS --
+CREATE TABLE recipe_ingredients (
+    id SERIAL PRIMARY KEY,
+    recipe_id INT REFERENCES recipes(id) ON DELETE CASCADE,
+    ingredient_id INT REFERENCES ingredients(id) ON DELETE CASCADE,
+    quantity NUMERIC NOT NULL,
+);
+
+ -- ALLERGENS --
+CREATE TABLE IF NOT EXISTS allergens (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL UNIQUE,
+);
+
+INSERT INTO allergens (name)
+VALUES ('Gluten'),
+       ('Skalldyr'),
+       ('Egg'),
+       ('Fisk'),
+       ('Peanøtter'),
+       ('Soya'),
+       ('Melk'),
+       ('Nøtter'),
+       ('Selleri'),
+       ('Sennep'),
+       ('Sesamfrø'),
+       ('Sulfitter'),
+       ('Lupin'),
+       ('Bløtdyr');
